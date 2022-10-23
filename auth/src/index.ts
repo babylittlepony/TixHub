7dea667779b69b2578af842850f55c3928d4bb6e
Egar <pramana.egar1540@gmail.com>
2022-10-23 10:01:44 +0700

    adding ingress + skaffold services and configured auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
+app.get("/api/users/currentuser", (req, res) => {
   res.send("YOOO");
 });
 
 app.listen(3000, () => {
-  console.log("Auth on 3000");
+  console.log("Auth on 3000, v6");
 });
