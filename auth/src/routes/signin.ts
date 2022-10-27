f1a82b4d0307fa1f8d57022a91932d6a6c7bdd6f
Egar <pramana.egar1540@gmail.com>
2022-10-27 19:55:01 +0700

    adding login feature with hashed password

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,18 @@
 import express from "express";
 
+import { BadRequestError } from "../errors/bad-request-error";
+import { User } from "../models/user";
+import { Password } from "../services/password";
+
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.json("Under construction");
+router.post("/api/users/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.find({ email });
+  if (!user) {
+    throw new BadRequestError("User not found, please sign up");
+  }
 });
 
 export { router as signInRouter };
