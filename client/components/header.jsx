5214579e48662b4c95d34ce7af59d3af83079731
Egar <pramana.egar1540@gmail.com>
2022-11-06 13:57:20 +0700

    completed authentication in client

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
 
 export default ({ currentUser }) => {
+  const links = [
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((link) => link)
+    .map(({ label, href }) => {
+      return (
+        <Link href={href}>
+          <p className="px-4">{label}</p>
+        </Link>
+      );
+    });
+
   return (
     <nav className="m-4 ">
-      <div className="container flex flex-wrap items-center justify-between">
+      <div className="container flex flex-wrap flex-row justify-between ">
         <Link href="/">TickHub</Link>
+        <div className="inline-flex">{links}</div>
       </div>
     </nav>
   );
