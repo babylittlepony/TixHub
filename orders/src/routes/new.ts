68228f5acea666d8f769d6ab982ae48b4b405d23
Egar <pramana.egar1540@gmail.com>
2022-11-22 08:06:59 +0700

    added ticket & order models, export to new order route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+import { requireAuth, validateRequest } from "@tixproject/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (res: Response, req: Request) => {
-  res.json();
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  body("ticketId")
+    .notEmpty()
+    .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // Check the Ticket id is valid Mongo id
+    .withMessage("Ticket id not found/provided"),
+  validateRequest,
+  async (res: Response, req: Request) => {
+    res.json();
+  }
+);
 
 export { router as newOrderRouter };
