5bc670901495ff9f7a58b0a090c58815485d81a8
Egar <pramana.egar1540@gmail.com>
2022-12-01 15:13:16 +0700

    added custom function in ticket model

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,10 +8,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const ticket = await Ticket.findOne({
-      _id: data.id,
-      version: data.version - 1,
-    });
+    const ticket = await Ticket.findByEvent(data); // Custom function, its search the ticket by id and version
 
     if (!ticket) {
       throw new Error("Ticket not found");
