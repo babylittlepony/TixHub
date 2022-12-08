a685d9595dccc892bd0bb8a95ce8ee63a79ded91
Egar <pramana.egar1540@gmail.com>
2022-12-08 07:58:45 +0700

    initial setup for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError, extractJwt } from "@tixproject/common";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+/*-----------Middleware-----------*/
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(extractJwt);
+/*-----------Middleware-----------*/
+
+/*-----------Routes-----------*/
+
+/*-----------Routes-----------*/
+
+/*-----------Error-----------*/
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+/*-----------Error-----------*/
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,51 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+import { createMongoId } from "@tixproject/common";
+
+declare global {
+  // Declaring global function
+  var signin: () => string[];
+}
+
+jest.mock("../nats-wrapper.ts");
+
+let mongo: any;
+// Start Mongo Memory Server first
+beforeAll(async () => {
+  jest.clearAllMocks();
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+
+  process.env.jwt_key = "supersecretkey";
+});
+// Delete each finished test
+beforeEach(async () => {
+  const collection = await mongoose.connection.db.collections();
+  collection.forEach(async (val) => await val.deleteMany({}));
+});
+// Close connection after finished all test
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  //Creating the global function
+  const payload = {
+    id: createMongoId(),
+    email: "test@test.com",
+  };
+
+  const token = jwt.sign(payload, process.env.jwt_key!); // Create the JWT
+  const session = { jwt: token }; // Build session object
+  const sessionJSON = JSON.stringify(session); // Turn the session to JSON
+  const base64 = Buffer.from(sessionJSON).toString("base64"); // Take JSON and encode it as Base64
+
+  return [`session=${base64}`];
+};
