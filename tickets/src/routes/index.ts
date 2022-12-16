be96685232927e296f15b1f933c4ce0c9623589d
Egar <pramana.egar1540@gmail.com>
2022-12-16 08:25:10 +0700

    display only available tickets

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from "express"
 
-import { Ticket } from "../models/ticket";
+import { Ticket } from "../models/ticket"
 
-const router = express.Router();
+const router = express.Router()
 
 router.get("/api/tickets", async (req: Request, res: Response) => {
-  const ticket = await Ticket.find();
-  res.json(ticket);
-});
+  const ticket = await Ticket.find({ orderId: undefined })
+  res.json(ticket)
+})
 
-export { router as ticketListRouter };
+export { router as ticketListRouter }
