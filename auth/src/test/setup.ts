853e11f7fca0d10996ec886df5b0d95cbf6e0e4c
Egar <pramana.egar1540@gmail.com>
2022-12-19 16:59:53 +0700

    test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,45 +1,43 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import request from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
+import request from "supertest"
 
-import { app } from "../app";
+import { app } from "../app"
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => Promise<string[]>
 }
 
-let mongo: any;
+let mongo: any
 // Start Mongo Memory Server first
 beforeAll(async () => {
-  const mongo = await MongoMemoryServer.create();
-  const mongoUri = mongo.getUri();
+  const mongo = await MongoMemoryServer.create()
+  const mongoUri = mongo.getUri()
 
-  await mongoose.connect(mongoUri, {});
+  await mongoose.connect(mongoUri, {})
 
-  process.env.jwt_key = "supersecretkey";
-});
+  process.env.jwt_key = "supersecretkey"
+})
 // Delete each finished test
 beforeEach(async () => {
-  const collection = await mongoose.connection.db.collections();
-  collection.forEach(async (val) => await val.deleteMany({}));
-});
+  const collection = await mongoose.connection.db.collections()
+  collection.forEach(async (val) => await val.deleteMany({}))
+})
 // Close connection after finished all test
 afterAll(async () => {
-  if (mongo) {
-    await mongo.stop();
-  }
-  await mongoose.connection.close();
-});
+  await mongo.stop()
+  await mongoose.connection.close()
+})
 
 global.signin = async () => {
-  const email = "test@gmail.com";
-  const password = "password";
+  const email = "test@gmail.com"
+  const password = "password"
 
   const response = await request(app)
     .post("/api/users/signup")
     .send({ email, password })
-    .expect(201);
-  const cookie = response.get("Set-Cookie");
+    .expect(201)
+  const cookie = response.get("Set-Cookie")
 
-  return cookie;
-};
+  return cookie
+}
